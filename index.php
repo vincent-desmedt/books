cb95f11a4a856b4d938c87ebddf2a03961c3e413
vincent-desmedt <vincent-desmedt@skynet.be>
2016-03-03 09:26:14 +0100

    list book database

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
+$dbConfig = parse_ini_file('db.ini');
+
+
 try {
-    $cn = new PDO('mysql:host=localhost;dbname=bibliographie','root','');
+    $dsn = sprintf('%s:host=%s;dbname=%s',$dbConfig['driver'],$dbConfig['host'],$dbConfig['dbname']);
+    $cn = new PDO($dsn, $dbConfig['username'], $dbConfig['password']);
     $cn->query('SET CHARACTER SET UTF8');
     $cn->query('SET NAMES UTF8');
 } catch(PDOException $e) {
     // quand on a un objet pour accèder aux méthodes on utilise ->
     echo $e->getMessage();
+}
+
+$booksStmnt = 'SELECT * from books';
+$pdoStmnt = $cn->query($booksStmnt);
+$books = $pdoStmnt->FetchAll();
+
+foreach ($books as $books) {
+    echo $books['title'].'<br>';
 }
